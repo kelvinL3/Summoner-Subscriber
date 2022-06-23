11ce29e757210056b6d6cb1797fa0f6483af6ed1
Kelvin Liu <kelvinliu1234@gmail.com>
2022-06-23 16:27:36 -0700

    secrets

--- riot/api.py
+++ riot/api.py
@@ -1,2 +1,21 @@
+#!/usr/bin/env python3
+
 # https://towardsdatascience.com/how-to-use-riot-api-with-python-b93be82dbbd6
 # https://cassiopeia.readthedocs.io/en/latest/
+
+# from cassiopeia import Summoner
+import cassiopeia as cass
+from .secrets import API_KEY, API_KEY2
+
+ANKUR = 'anchor1'
+ANKUR_ALT = 'anchor2'
+
+REGION = 'NA'
+
+# def get_player()
+# pass
+
+cass.set_riot_api_key(API_KEY)
+
+anchor = cass.Summoner(name=ANKUR, region=REGION)
+# kalturi.match_history
