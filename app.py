4ba91b190fafb38a9cdd1ac9ce0e264d55210677
Kelvin Liu <kelvinliu1234@gmail.com>
2022-06-23 23:30:03 -0700

    oops

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ import logger
 victim = 'anchor1'
 
 if __name__ == "__main__":
-	ankur = riot.get_summoner(victim)
-	# import ipdb; ipdb.set_trace()
-	current_match = riot.get_current_match(ankur)
+    ankur = riot.get_summoner(victim)
+    # import ipdb; ipdb.set_trace()
+    current_match = riot.get_current_match(ankur)
 
-	if current_match:
-		logger.log("Ankur is currently in game")
-		ankur_participant = riot.find_participant_in_match(
-			current_match, ankur)
-		logger.log(f"Ankur is playing {ankur_participant.champion.name}")
-		rank = riot.get_solo_queue_rank()
-		logger.log(f"Ankur's current rank is {rank[0]} {rank[1]}")
-	else:
-		logger.log("Ankur is currently not in game")
+    if current_match:
+        logger.log("Ankur is currently in game")
+        ankur_participant = riot.find_participant_in_match(
+            current_match, ankur)
+        logger.log(f"Ankur is playing {ankur_participant.champion.name}")
+        rank = riot.get_solo_queue_rank(ankur.ranks)
+        logger.log(f"Ankur's current rank is {rank[0]} {rank[1]}")
+    else:
+        logger.log("Ankur is currently not in game")
