016523f3bf571ec9556713e70eb593b1df172215
Kelvin Liu <kelvinliu1234@gmail.com>
2022-06-24 11:43:23 -0700

    add timestamp and timezone to logs

--- logger.py
+++ logger.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import pytz
 from pathlib import Path
 
 BASE_DIR = Path(os.getenv("LOG_DIRECTORY"))
@@ -9,7 +10,9 @@ def current_date_as_str() -> str:
     return datetime.today().strftime("%Y-%m-%d")
 
 
-def log(line: str) -> None:
+def log(line: str, withTimeStamp: bool = True) -> None:
     with open(BASE_DIR / f"{current_date_as_str()}.log", "a") as f:
+        if withTimeStamp:
+            f.write(str(datetime.now(pytz.utc)))
         f.write(line)
         f.write("\n")
