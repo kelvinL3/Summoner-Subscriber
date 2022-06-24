bd6b344cb308c46ac7dbca44c6e38ec145b273ce
Kelvin Liu <kelvinliu1234@gmail.com>
2022-06-23 23:22:22 -0700

    secret

---
+++ discord/api.py
@@ -0,0 +1,7 @@
+import discord
+import os
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
--- secrets.sh
+++ secrets.sh
@@ -1,2 +1,3 @@
 export RIOT_API_KEY="<put something here>"
 export LOG_DIRECTORY="<put something here>"
+export DISCORD_TOKEN="<put something here>"
