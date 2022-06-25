c06fc937fabcc6c05416b7da8b8602fb6e7645d4
Aaron Kau <akau@quora.com>
2022-06-25 16:48:48 -0700

    change to single discord connection

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
 from riot import api as riot
 import logger
-from discord_api.api import change_name
+from discord_api import api as discord
+import asyncio
 
 victim = "anchor1"
 
-if __name__ == "__main__":
-    ankur = riot.get_summoner(victim)
-    current_match = riot.get_current_match(ankur)
-    rank = riot.get_solo_queue_rank(ankur.ranks)
+async def run_loop():
+    await discord.connect()
 
-    if current_match:
-        logger.log("Ankur is currently in game")
-        ankur_participant = riot.find_participant_in_match(current_match, ankur)
-        logger.log(f"Ankur is playing {ankur_participant.champion.name}")
+    while True:
+        ankur = riot.get_summoner(victim)
+        current_match = riot.get_current_match(ankur)
+        rank = riot.get_solo_queue_rank(ankur.ranks)
         logger.log(f"Ankur's current rank is {rank[0]} {rank[1]}")
-    else:
-        logger.log("Ankur is currently not in game")
+        if current_match:
+            logger.log("Ankur is currently in game")
+            ankur_participant = riot.find_participant_in_match(current_match, ankur)
+            logger.log(f"Ankur is playing {ankur_participant.champion.name}")
+        else:
+            logger.log("Ankur is currently not in game")
 
-    change_name("Hec", rank[0], rank[1])
-    print("Done")
+        logger.log("===========")
+        await discord.change_name("Hec", rank[0], rank[1])
+        print("Done")
+        print()
+        await asyncio.sleep(60)
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(run_loop())
--- discord_api/api.py
+++ discord_api/api.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import re
+import logger
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -10,32 +11,30 @@ if not TOKEN:
 GUILD_ID = 706426037415837698
 ANKUR_NAME = "the kid#7020"
 
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
-def change_name(champion: str, rank: str, division: str):
-    intents = discord.Intents.default()
-    intents.members = True
+async def connect():
+    await client.start(TOKEN)
+    print("Connected")
 
-    client = discord.Client(intents=intents)
 
-    @client.event
-    async def on_ready():
-        print(client.guilds)
-        monarcho_guild = [
-            guild for guild in client.guilds if guild.id == GUILD_ID
-        ][0]
+async def change_name(champion: str, rank: str, division: str):
+    print(client.guilds)
+    monarcho_guild = [
+        guild for guild in client.guilds if guild.id == GUILD_ID
+    ][0]
 
-        ankur_member = monarcho_guild.get_member_named(ANKUR_NAME)
+    ankur_member = monarcho_guild.get_member_named(ANKUR_NAME)
+    ankur_display_name = ankur_member.display_name
+    logger.log(f"Ankur's current Discord name: {ankur_display_name}")
 
-        ankur_display_name = ankur_member.display_name
+    # reg = re.search("(.+)-\s*(\d+games\s+\w+)", ankur_display_name)
 
-        # reg = re.search("(.+)-\s*(\d+games\s+\w+)", ankur_display_name)
-
-        # await ankur_member.edit(nick=ankur_display_name + ";")
-        # try:
-        # normal, status = reg.groups()
-        # await ankur_member.edit(nick=f"{normal} - {rank} {division}")
-        # except :
-        #     raise
-        exit(0)
-
-    client.run(TOKEN)
+    # await ankur_member.edit(nick=ankur_display_name + ";")
+    # try:
+    # normal, status = reg.groups()
+    # await ankur_member.edit(nick=f"{normal} - {rank} {division}")
+    # except :
+    #     raise
